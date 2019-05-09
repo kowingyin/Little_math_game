08a4b25f1b770c40e87170ce398e6785fe726e5e
kowingyin <ken402402@gmail.com>
2019-05-10 06:41:47 +0800

    debug

--- Assignment/app/src/main/java/com/example/timmy/myapplication/BrainyGameActivity.java
+++ Assignment/app/src/main/java/com/example/timmy/myapplication/BrainyGameActivity.java
@@ -1,11 +1,13 @@
 package com.example.timmy.myapplication;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class BrainyGameActivity extends AppCompatActivity {
-
+    private static final int REQUEST_CODE = 3434;
     private String DB_PATH;
     private String DB_NAME = "MathGame_DB.db";
     @Override
@@ -14,6 +16,26 @@ public class BrainyGameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_brainy_game);
         final Context context = this;
         DB_PATH = context.getFilesDir().getPath() +"/db/";
+    }
+    public void onClickBtnStartSpelling(View v){
+        Intent i = new Intent(this, SpellingGame.class);
+        startActivityForResult(i, REQUEST_CODE);
+    }
+    public void onClickBtnStartFlash(View v){
+        Intent i = new Intent(this, MathematicGame.class);
+        startActivityForResult(i, REQUEST_CODE);
+    }
+    public void onClickBtnStartShapes(View v){
+        Intent i = new Intent(this, MathematicGame.class);
+        startActivityForResult(i, REQUEST_CODE);
+    }
+    public void onClickBtnStartReaction(View v){
+        Intent i = new Intent(this, MathematicGame.class);
+        startActivityForResult(i, REQUEST_CODE);
+    }
+    public void onClickBtnStartColors(View v){
+        Intent i = new Intent(this, MathematicGame.class);
+        startActivityForResult(i, REQUEST_CODE);
     }
 
 }
